46daadcb1a0a15d86e62d0cbfd8d6e14f8798d41
Mauritsmajaz <44691186+Mauritsmajaz@users.noreply.github.com>
2018-11-02 11:29:45 +0700

    Add files via upload

---
+++ Directive/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './Basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './Better-highlight/better-highlight.directive';
+import { UnlessDirective } from './Unless.directive';
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      BasicHighlightDirective,
+      BetterHighlightDirective,
+      UnlessDirective
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
